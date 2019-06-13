a2fb1a4bf41ccdb3d51e7750612311c5dedc1fba
hyc <919473496@qq.com>
2019-06-13 15:00:36 +0800

    把工程传到本地仓库

---
+++ gie-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+import sun.applet.Main;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
